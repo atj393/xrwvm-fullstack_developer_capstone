0fbc754a4b384ba42acbd573189589bafc863658
Alexis Johnson <alexistobyjr@gmail.com>
2024-07-08 23:50:38 +0200

    Module 2

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,6 +13,9 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.contrib.auth import logout
+
 # from .populate import initiate
 
 
@@ -22,13 +25,14 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
+
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
     # Get username and password from request.POST dictionary
     data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
+    username = data["userName"]
+    password = data["password"]
     # Try to check if provide credential can be authenticated
     user = authenticate(username=username, password=password)
     data = {"userName": username}
@@ -38,14 +42,40 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_user(request):
+    logout(request)
+    data = {"userName": ""}
+    return JsonResponse(data)
+
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
-# def registration(request):
-# ...
+def register_user(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        username = data["userName"]
+        password = data["password"]
+        first_name = data["firstName"]
+        last_name = data["lastName"]
+        email = data["email"]
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({"status": False, "error": "Already Registered"})
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+        )
+        user.save()
+        login(request, user)
+        return JsonResponse({"status": True, "userName": username})
+    return JsonResponse({"status": False, "error": "Invalid request"})
+
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
